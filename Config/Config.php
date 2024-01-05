d9c38382c5117b7da20e35629c01b485119bb824
Fernando Q <vfqs94@gmail.com>
2024-01-05 16:56:45 -0400

    Update Config.php
    exam

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,6 @@
 <?php
-const BASE_URL = "https://www.tecnoweb.org.bo/inf513/grupo15sa/proyecto2/Tienda/";
+//const BASE_URL = "https://www.tecnoweb.org.bo/inf513/grupo15sa/proyecto2/Tienda/";tienda-main
+const BASE_URL = "https://www.tecnoweb.org.bo/inf513/grupo15sa/proyecto2/tienda-main/";
 const HOST = "mail.tecnoweb.org.bo";
 const USER = "grupo15sa";
 const PASS = "grup015grup015";
